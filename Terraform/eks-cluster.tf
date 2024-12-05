53ca7768f61632a7bbfa82bc2325b6bb2eb79268
akshay-p-chung <101680252+akshay-p-chung@users.noreply.github.com>
2024-12-06 00:46:19 +0530

    Update eks-cluster.tf

--- Terraform/eks-cluster.tf
+++ Terraform/eks-cluster.tf
@@ -1,13 +1,13 @@
 #EKS
 
 module "eks" {
-    source "terraform-aws-modules/eks/aws"
+	source = "terraform-aws-modules/eks/aws"
 
-	cluster name    = var.cluster name
+	cluster_name    = var.cluster_name
 	cluster_version = var.kubernetes_version
 
-	iam_role_additional policies = {
-		ElasticLoadBalancingFullAccess "arniaws:iam::aws:policy/ElasticLoad BalancingFullAccess"
+	iam_role_additional_policies = {
+		ElasticLoadBalancingFullAccess = "arniaws:iam::aws:policy/ElasticLoad BalancingFullAccess"
 	}
 	
 	vpc_id = var.vpc_id
@@ -21,7 +21,7 @@ module "eks" {
 		instance_types = var.instance_types
 		create_node_security_group = false
 			iam_role_additional_policies = {
-				AmazonS3FullAccess = "arn:aws:iam::aws:policy/Amazon53FullAccess",
+				AmazonS3FullAccess = "arn:aws:iam::aws:policy/AmazonS3FullAccess",
 				AutoScalingFullAccess = "arn:aws:iam::aws:policy/AutoScalingFullAccess",
 				SecretsManagerReadWrite = "arn:aws:iam::aws:policy/SecretsManagerReadWrite",
 				CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
@@ -42,7 +42,7 @@ module "eks" {
 			principal_arn = "arn:aws:iam::943535361612:role/ADMIN"
 			policy_associations = {
 				ex = {
-					policy_arn "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy" 
+					policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy" 
 					access_scope {
 						type = "cluster"
 					}
@@ -50,7 +50,7 @@ module "eks" {
 			}
 		}
 		jenkins_server = {
-			principal_arn = "arn:aws:iam::943535361612:role/Jenkins-iam-role"
+			principal_arn = "arn:aws:iam::058264456163:instance-profile/Jenkins-IAM-Role"
 			policy_associations = {
 				ex = {
 					policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
